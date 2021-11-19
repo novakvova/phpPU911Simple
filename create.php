6682e7d1af960ee425c8db2d8ce9fb117860ea07
novakvova <novakvova@gmail.com>
2021-11-19 10:33:41 +0200

    Upload image crate news

--- create.php
+++ create.php
@@ -4,12 +4,16 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 //    echo "<h3>{$_POST['name']}</h3>";
 //    echo "<h3>{$_POST['image']}</h3>";
 //    echo "<h3>{$_POST['description']}</h3>";
+
+    $filename = uniqid().'.jpg';
+    $filesavepath=$_SERVER['DOCUMENT_ROOT'].'/images/'.$filename;
+    move_uploaded_file($_FILES['image']['tmp_name'],$filesavepath);
+
     $conn = new PDO("mysql:host=localhost;dbname=local.pu911.com", "root", "");
     $sql = "INSERT INTO `news` (`name`, `image`, `description`) VALUES (?, ?, ?);";
     $name=$_POST['name'];
-    $image = $_POST['image'];
     $description=$_POST['description'];
-    $conn->prepare($sql)->execute([$name,$image,$description]);
+    $conn->prepare($sql)->execute([$name,$filename,$description]);
     header("Location: /");
     exit();
 }
@@ -43,17 +47,23 @@ $t=56;
         </div>
         <div class="row">
             <div class="col-8">
-                <form method="post">
-                    <div class="form-group">
-                        <label for="name">Name</label>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
                         <input name="name" id="name" type="text" class="form-control" />
                     </div>
-                    <div class="form-group">
-                        <label for="image">Фото</label>
-                        <input type="text" name="image" id="image" class="form-control" />
+                    <div class="mb-3">
+                        <label for="image" class="form-label">
+                            <img src="https://www.pngall.com/wp-content/uploads/2/Upload-Transparent.png"
+                                width="150"
+                                 id="img_preview"
+                                style="cursor: pointer"
+                            />
+                        </label>
+                        <input type="file" name="image" id="image" class="form-control d-none" />
                     </div>
-                    <div class="form-group">
-                        <label for="description">Опис</label>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Опис</label>
                         <textarea type="text" name="description" id="description" rows="10" cols="45" class="form-control"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary mb-3">Add new</button>
@@ -63,5 +73,14 @@ $t=56;
     </div>
 </div>
 <script src="/js/bootstrap.bundle.min.js"></script>
+<script>
+    window.addEventListener('load',function() {
+       const file = document.getElementById('image');
+       file.addEventListener("change", function(e) {
+           const uploadFile = e.currentTarget.files[0];
+           document.getElementById("img_preview").src=URL.createObjectURL(uploadFile);
+       });
+    });
+</script>
 </body>
 </html>
